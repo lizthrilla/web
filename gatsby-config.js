293ffb0cfefe92348f9854f337b16c616dd68981
Jason L Perry <jason@suncoast.io>
2019-08-01 09:24:35 -0400

    Remove deprecated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,7 +103,6 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 1035,
-              sizeByPixelDensity: true,
             },
           },
         ],
