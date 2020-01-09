2b3ff689fc00eafb0326435c3c7f8fd873eaf82f
Jason L Perry <jason@suncoast.io>
2020-01-09 15:10:26 -0500

    Update program schedule, closes #302

--- src/components/academy/ProgramSchedule.js
+++ src/components/academy/ProgramSchedule.js
@@ -12,30 +12,41 @@ const ProgramSchedule = () => (
             <th>Status</th>
             <th>Classes Start</th>
             <th>Graduation / Demo Day</th>
+            <th>Back-end</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Cohort XV</td>
+            <td>Cohort 16</td>
             <td>
               <em>In Progress</em>
             </td>
-            <td>July 29, 2019</td>
-            <td>October 18, 2019</td>
+            <td>October 28, 2019</td>
+            <td>January 31, 2020</td>
+            <td>.NET</td>
           </tr>
           <tr className="upcoming">
-            <td>Cohort 16</td>
+            <td>Cohort 17</td>
             <td>
               <Link to="/academy/apply">Now Enrolling</Link>
             </td>
-            <td>October 28, 2019</td>
-            <td>January 31, 2020</td>
+            <td>February 10, 2020</td>
+            <td>April 30, 2020</td>
+            <td>.NET</td>
           </tr>
           <tr>
-            <td>Cohort 17</td>
+            <td>Cohort 18</td>
             <td>"</td>
-            <td>February 2020</td>
-            <td>Spring 2020</td>
+            <td>May 10, 2020</td>
+            <td>July 31, 2020</td>
+            <td>.NET</td>
+          </tr>
+          <tr>
+            <td>Cohort 19</td>
+            <td>"</td>
+            <td>August 10, 2020</td>
+            <td>November 2020</td>
+            <td>TBD</td>
           </tr>
         </tbody>
       </table>
