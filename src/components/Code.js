d48048a6741359a6e48705d57cf8e053ba0917a3
Gavin Stark <gavin@gstark.com>
2020-03-04 16:08:00 -0500

    Fixes code formatting and adds a COPY action

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/github'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const Code = ({ codeString, language, ...props }) => {
+  const [copied, setCopied] = useState(false)
+
+  const languageMap = {
+    'C#': 'cpp',
+    js: 'javascript',
+    HTML: 'html'
+  }
+
   return (
     <Highlight
       {...defaultProps}
       code={codeString}
-      language={language}
-      theme={undefined}
+      language={languageMap[language] || language}
+      theme={theme}
     >
-      {({ className, tokens, getLineProps, getTokenProps }) => (
-        <div className="gatsby-highlight">
-          <pre className={className}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <div className="gatsby-highlight" style={{ position: 'relative' }}>
+          <CopyToClipboard text={codeString} onCopy={() => setCopied(true)}>
+            <pre
+              style={{
+                cursor: 'pointer',
+                position: 'absolute',
+                top: 0,
+                right: 0
+              }}
+            >
+              {copied ? 'COPIED!' : 'COPY'}
+            </pre>
+          </CopyToClipboard>
+          <pre className={className} style={style}>
             {tokens.map((line, i) => (
               <div {...getLineProps({ line, key: i })}>
                 {line.map((token, key) => (
