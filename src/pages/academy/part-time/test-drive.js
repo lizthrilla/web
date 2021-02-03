06a04eb221a37f779b9217b3dac64c3efc8f3360
Jason L Perry <jason@suncoast.io>
2021-02-03 15:41:26 -0500

    Update
    A lazy commit.

--- src/pages/academy/part-time/test-drive.js
+++ src/pages/academy/part-time/test-drive.js
@@ -23,13 +23,13 @@ const TestDriveCourse = ({ data }) => (
           <div className="level-item has-text-centered">
             <div>
               <p className="heading">Start Date</p>
-              <p className="title is-5">September 19, 2020</p>
+              <p className="title is-5">February 20, 2021</p>
             </div>
           </div>
           <div className="level-item has-text-centered">
             <div>
               <p className="heading">End Date</p>
-              <p className="title is-5">October 24, 2020</p>
+              <p className="title is-5">March 27, 2021</p>
             </div>
           </div>
           <div className="level-item has-text-centered">
